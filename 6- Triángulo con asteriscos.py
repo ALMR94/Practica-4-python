8b3ff3b4458948deb20ac6b7206b3592ea9bc46c
ALMR94 <32308491+ALMR94@users.noreply.github.com>
2017-10-25 19:27:28 +0200

    Add files via upload

---
+++ 6- Triángulo con asteriscos.py
@@ -0,0 +1,9 @@
+# -*- coding: cp1252 -*-
+"""Antonio Li Manzaneque- 1º DAW - Práctica 4 - Ejercicio 6 - Escriu un programa que demani l'alçada d'un triangle i ho dibuixi de la següent manera:"""
+print "Escribe la altura del triángulo:"
+a=(int(raw_input()))
+
+for n in range(1,(a+1)):
+    print n*"*"
+
+    
