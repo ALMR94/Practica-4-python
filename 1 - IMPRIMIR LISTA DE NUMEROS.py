d51cb8f344f55a45e2af947b849cebb6e97c8e3a
ALMR94 <32308491+ALMR94@users.noreply.github.com>
2017-10-25 19:26:34 +0200

    Add files via upload

---
+++ 1 - IMPRIMIR LISTA DE NUMEROS.py
@@ -0,0 +1,18 @@
+﻿"""Antonio Li Manzaneque- 1º DAW - Práctica 4 - Ejercicio 1 - Escriu un programa que escrigui els següents nombres (la separació entre nombre és per a
+facilitar-te sabre quants de nombres s'ha d'escriure en cada bucle) i en el que la funció range que
+utilitzis tengui un únic argument (per exemple, per a la primera llista range(10))."""
+for a in range(10):
+    print a+1,
+print 
+for b in range(10):
+    print (b+1)*2,
+print 
+for c in range(10):
+    print (c+10)*2,
+print
+for d in range(6):
+    print (d+10+d*3),
+print
+for e in range(9):
+    print (45-(5*(e+1))),
+                          
