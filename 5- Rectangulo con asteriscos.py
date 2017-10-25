47897121eb2340d6aed5b75b1c83c746257ffb75
ALMR94 <32308491+ALMR94@users.noreply.github.com>
2017-10-25 19:27:17 +0200

    Add files via upload

---
+++ 5- Rectangulo con asteriscos.py
@@ -0,0 +1,13 @@
+# -*- coding: cp1252 -*-
+"""Antonio Li Manzaneque- 1º DAW - Práctica 4 - Ejercicio 5 - Escriu un programa que demani l'amplada i alçada d'un rectangle i ho dibuixi de la següent
+manera:"""
+print "Escribe la anchura del rectángulo:"
+a=(int(raw_input()))
+print "Escribe la altura del rectángulo:"
+b=(int(raw_input()))
+
+for i in range(b):
+    print "*"*a
+
+
+    
