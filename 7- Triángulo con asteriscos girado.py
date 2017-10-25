e75e9f78efda6b916f4d2af457fe2a3288d115c5
ALMR94 <32308491+ALMR94@users.noreply.github.com>
2017-10-25 19:27:43 +0200

    Add files via upload

---
+++ 7- Triángulo con asteriscos girado.py
@@ -0,0 +1,7 @@
+# -*- coding: cp1252 -*-
+"""Antonio Li Manzaneque- 1º DAW - Práctica 4 - Ejercicio 7 - Escriu un programa que demani l'alçada d'un triangle i ho dibuixi de la següent manera:"""
+print "Escribe la altura del triángulo:"
+a=(int(raw_input()))
+
+for n in range((a),0,-1):
+    print n*"*"
